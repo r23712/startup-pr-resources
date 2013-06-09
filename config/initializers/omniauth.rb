577a00f3b465b6c7c5c6bab6c454f3387542ed2c
r23712 <ryan.n.oliver@gmail.com>
2013-06-09 04:13:08 -0400

    omniauth tokens out

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth:: Builder do
   # provider :developer unless Rails.evn.production?
-  provider :twitter, 'onlOxrec55Ycat3Kc6J3w', 'ph3vd13QzOWduEuqmS5Qsqwi7mzkBfDihek8VUpvew'
-  provider :facebook, '268087800002654', '094c99cfd7931f0323363e94b8150d53'
+  provider :twitter, '', ''
+  provider :facebook, '', ''
 end
